caff022ff91a6c2e903a681b3b415a686c1cfaa1
Varghese Bobus <varghesebobus@gmail.com>
2021-08-27 01:02:03 -0700

    initial commit dog fetching game

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float nextDogSpawn;
+
+    private void Start()
+    {
+        nextDogSpawn = Time.time + 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog(after every 1 second only)
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextDogSpawn)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            nextDogSpawn += 1.0f;
+        }
+    }
+}
